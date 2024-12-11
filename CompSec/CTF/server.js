48be42564a4c85d0c6df70f930e109a3a0d199bd
ravkumamogus <r2a3v5i7.kumar@gmail.com>
2024-12-10 18:00:13 -0800

    debug

--- CompSec/CTF/server.js
+++ CompSec/CTF/server.js
@@ -15,7 +15,7 @@ const flag = 'how_to_hack_appel?'
 const bonus = 'sofihs!'
 // Middleware to set Content Security Policy
 app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';");
+    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline';");
     next();
 });
 
